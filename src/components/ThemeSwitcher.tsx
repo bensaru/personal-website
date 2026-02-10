5ebc99989728c7ed628aa13aa3b5653f20c473f3
bensaru <cgwpkd636@gmx.us>
2026-02-10 15:50:50 +0900

    change some texts

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -7,7 +7,7 @@ export default function ThemeSwitcher() {
       className="absolute top-6 md:left-[7%] lg:left-[10%] xl:left-[24%] left-[2%] z-[9999] cursor-pointer backdrop-blur-xl rounded-full"
       onClick={() => setTheme(activeTheme)}
     >
-      {activeTheme === "dark" ? (
+      {activeTheme === "light" ? (
         <img alt="" src="/svgs/moon.svg" className="w-[40px] h-[40px]" />
       ) : (
         <img alt="" src="/svgs/sun.svg" className="w-[40px] h-[40px]" />
