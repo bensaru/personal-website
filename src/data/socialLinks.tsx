f96f6e7118bff20dd9763831120435de9fd2356e
bensaru <vensakai1030@gmail.com>
2026-02-20 16:10:05 +0900

    add x link

--- src/data/socialLinks.tsx
+++ src/data/socialLinks.tsx
@@ -2,5 +2,5 @@ export const socialLinks = {
   github: "https://github.com/bensaru",
   discord: "https://discordapp.com/users/341254939127054337",
   telegram: "https://t.me/bensaru7",
-  x: "",
+  x: "https://x.com/bensaru7",
 };
